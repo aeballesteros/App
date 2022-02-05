4cab4662327967fa6db460fc54c737986726c005
Atilio Emmanuel Ballesteros <atilio.emm.b@gmail.com>
2022-02-05 09:06:19 -0300

    Create home component (home.js

--- src/components/home.js
+++ src/components/home.js
@@ -1,12 +1,28 @@
 import React, {Component} from "react";
-import Form from 'react-bootstrap/Form';
+import {Form, Button, Container} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default class Home extends Component{
     render(){
         return(
-            <div className="form">
-                <h1>Hola</h1>
-            </div>
+            <Container>
+                <Form>
+                    <Form.Group className="mb-1" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeHolder="Enter email"/>
+                        <Form.Text className="text-muted">
+                            We'll never share your email with anyone else
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeHolder="Password"/>
+                    </Form.Group>
+
+                    <Button variant="primary" type="submit">LogIn</Button>
+                </Form>
+            </Container>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Home from './components/home';
 
-
-
 ReactDOM.render( 
     <Home />,
   document.getElementById('root')
