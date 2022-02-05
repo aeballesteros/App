be600f1e66791dc079cd35143a9bdcb3c084a7c0
Atilio Emmanuel Ballesteros <atilio.emm.b@gmail.com>
2022-02-05 08:21:14 -0300

    Project creation with create-react-app

---
+++ src/components/home.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import Form from 'react-bootstrap/Form';
+
+export default class Home extends Component{
+    render(){
+        return(
+            <div className="form">
+                <h1>Hola</h1>
+            </div>
+        )
+    }
+}
